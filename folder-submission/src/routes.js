002c442e19db443a8e82e9880cccb3db790b0061
josua-stng <josuasitanggang51@gmail.com>
2022-02-14 11:10:15 +0700

    add file

---
+++ folder-submission/src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBookHandler,
+  getAllBook,
+  getBookWithIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBook,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookWithIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
